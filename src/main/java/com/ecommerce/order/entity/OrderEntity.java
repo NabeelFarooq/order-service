4c654516ae7e45c6fbb2e38c4817055b30492d4c
NabeelFarooq <nabeel007dar@gmail.com>
2026-09-23 14:43:04 +0530

    create order entity class with required fields

--- src/main/java/com/ecommerce/order/entity/OrderEntity.java
+++ src/main/java/com/ecommerce/order/entity/OrderEntity.java
@@ -4,32 +4,68 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class OrderEntity {
-	Request:
-	{
-	    "customerId": 101,
-	    "customerName": "Rahul",
-	    "productId": 501,
-	    "productName": "Laptop",
-	    "quantity": 1,
-	    "amount": 75000,
-	    "deliveryAddress": "Bangalore"
+	private int orderId;
+	private int customerId;
+	private int productId;
+	private String customerName;
+	private String productName;
+	private int quantity;
+	private double amount;
+	private String deliveryAddress;
+	private String status;
+	public int getOrderId() {
+		return orderId;
 	}
-
-	Response:
-	{
-	    "orderId": 1001,
-	    "status": "CREATED"
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
 	}
-
-	Topic: order-created
-
-	Event:
-	{
-	    "eventId": "EVT-10001",
-	    "eventType": "ORDER_CREATED",
-	    "orderId": 1001,
-	    "customerId": 101,
-	    "amount": 75000,
-	    "deliveryAddress": "Bangalore"
+	public int getCustomerId() {
+		return customerId;
 	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getDeliveryAddress() {
+		return deliveryAddress;
+	}
+	public void setDeliveryAddress(String deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 }
