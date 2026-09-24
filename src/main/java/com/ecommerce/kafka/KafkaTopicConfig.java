ccc5c0bb53c382a7c509e1e4fa1866f37791dd1c
NabeelFarooq <nabeel007dar@gmail.com>
2026-09-24 09:18:21 +0530

    kafka topic config

---
+++ src/main/java/com/ecommerce/kafka/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.ecommerce.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic orderCreatedTopic() {
+        return new NewTopic("order-created", 3, (short) 1);
+    }
+}
